7706b93280b99a4c6b59b74b1600eaba1bebae5c
Ozan Kaya <ozankaya673@gmail.com>
2025-01-30 18:51:14 +0300

    Add files via upload

--- train_test-lgbm.py
+++ train_test-lgbm.py
@@ -1,16 +1,24 @@
-import lightgbm as lgb
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+
+import lightgbm as lgb
 
 import pickle
 
-import matplotlib.pyplot as plt
-
 def get_filters(df: pd.DataFrame,
                 columns: list[str]) -> list[dict]:
+    """Return unique value combinations of a DataFrame.
+
+    'columns' is the list of names of the columns
+    that will be searched for unique values.
+    """
     
+    # Select columns and drop duplicates
+    # which will leave only unique rows.
     df_unique = df[columns].drop_duplicates()
 
+    # Loop through unique rows and add them to the list.
     filter_list = []
     for i in range(len(df_unique)):
         filter_list.append(df_unique.iloc[i].to_dict())
@@ -57,75 +65,100 @@ def date_to_features(df: pd.DataFrame) -> pd.DataFrame:
 
     return df_features
 
+def add_lag_and_window(df: pd.DataFrame,
+                       target_column: str,
+                       lags: int = 1,
+                       window_size: int = 4) -> pd.DataFrame:
+    """ Add lag and mean of rolling window of a target column."""
 
+    df_result = df.copy()
+
+    # The values of target column are shifted by lag times and added to rows.
+    for lag in range(1,lags+1):
+        df_result[f'lag_{lag}'] = df_result.loc[:,target_column].shift(lag)
+
+    # Mean of the rolling window of size 'window_size' is added to rows.
+    df_result['rolling_window'] = df_result.loc[:,target_column].rolling(window_size).mean().shift()
+
+    return df_result
+
+# Read time series data
 data = pd.read_pickle('Sales')
 
+# Determine filtering columns and columns to exclude as feature
 columns_to_exclude = ['Date','Quantity']
 columns_to_filter = [col for col in data.columns.tolist()
                      if col not in columns_to_exclude]
 columns_to_exclude.extend(columns_to_filter)
 
-filter_list = get_filters(data,columns_to_filter)
-
+# Determine filters to examine data, add date features
+filter_list = get_filters(data, columns_to_filter)
 data = date_to_features(data)
 
+# Split data into train and test sets
 train_size = 0.8
 dates = data['Date'].unique()
 train_enddate = dates[int(0.8*len(dates))]
-
 train_data = data.loc[data['Date'] < train_enddate]
 test_data = data.loc[data['Date'] >= train_enddate]
+
+# Gather test dates for plotting
 test_dates = data['Date'].loc[data['Date'] >= train_enddate].unique()
 
-lag = 1
-window = 4
-
 models = []
+# Loop through filters
 for filterKeys in filter_list:
-    
+    # Filter by each filter combination
     mask = np.logical_and.reduce(
         [train_data[key] == value for key, value in filterKeys.items()])
-    
     train_data_filtered = train_data[mask]
-    train_data_filtered[f'lag_{lag}'] = train_data_filtered.loc[:,'Quantity'].shift(lag)
-    train_data_filtered[f'rolling_{window}'] = train_data_filtered.loc[:,'Quantity'].rolling(window).mean().shift()
     
+    # Add lag and window features
+    train_data_filtered = add_lag_and_window(train_data_filtered, target_column='Quantity')
+    
+    # Split the train data into features and target
     X_train = train_data_filtered[[col for col in train_data_filtered.columns
                                     if col not in columns_to_exclude]]
     y_train = train_data_filtered['Quantity']
 
+    # Initiate and fit the model
     model = lgb.LGBMRegressor()
     model.fit(X_train,y_train)
 
+    # Initiate the plot
     figure, ax = plt.subplots(figsize=(10, 5))
     
+    # Now filter the test set with the same filters
     mask = np.logical_and.reduce(
         [test_data[key] == value for key, value in filterKeys.items()])
-    
     test_data_filtered = test_data[mask]
-    test_data_filtered[f'lag_{lag}'] = test_data_filtered.loc[:,'Quantity'].shift(lag)
-    test_data_filtered[f'rolling_{window}'] = test_data_filtered.loc[:,'Quantity'].rolling(window).mean().shift()
+
+    test_data_filtered = add_lag_and_window(test_data_filtered, target_column='Quantity')
     
     X_test = test_data_filtered[[col for col in test_data_filtered.columns
                                  if col not in columns_to_exclude]]
     
+    # Predict and store them in a DataFrame for plotting
     preds = model.predict(X_test)
-    
     df_plot = pd.DataFrame({'date': test_dates,
                             'pred': preds})
     
+    # Filter the whole data, send it to the plot
     mask = np.logical_and.reduce(
         [data[key] == value for key, value in filterKeys.items()])
-    
     data[mask][['Date','Quantity']].plot(ax=ax, label='Actual', x='Date',y='Quantity')
+
+    # Add the prediction to the plot
     df_plot.plot(ax=ax, label='Prediction', x='date', y='pred')
 
+    # Arrange the legend and the title, and print the plot
     plt.legend(['Actual', 'Prediction'])
     plt.title(f'{'-'.join([value for value in filterKeys.values()])}')
     plt.show()
 
+    # Save the model with its corresponding filters
     models.append([filterKeys, model])
 
-
+# Save all models locally
 with open('models', mode='wb') as f:
-    pickle.dump(models,f)
+    pickle.dump(models, f)
